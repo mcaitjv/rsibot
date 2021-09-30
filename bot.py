c24e5174a8add7789d7faaceaf88e628785c54f2
mcaitjv <mucahitk@gmail.com>
2021-09-30 22:34:29 +0300

    init

--- bot.py
+++ bot.py
@@ -6,14 +6,37 @@ import sqlalchemy
 import datetime
 from time import time, sleep
 import re,requests
+import sqlite3
+import random
+
+ORDER_TYPE_MARKET = 'MARKET'
+SIDE_BUY = 'BUY'
+TRADE_SYMBOL = 'USDTTRY'
+TRADE_QUANTITY = 3
 
 
-client = Client(config.API_KEY, config.API_SECRET)
+def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
+    try:
+        print("sending order")
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        print(order)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+
+    return True
+
+client = Client(config.API_KEY, config.API_SECRET, tld = "com")
 engine = sqlalchemy.create_engine('sqlite:///tether_usdtry.db')
 
+conn = sqlite3.connect("tether_usdtry.db")
+
+n = random. randint(0,22)
+
 while True:
+    n = random. randint(45,59)
     closes = []
-    sleep(60 - time() % 60)
+    sleep(n - time() % n)
     usdttry_price = client.get_symbol_ticker(symbol="USDTTRY")
     now = datetime.datetime.now().strftime("%H:%M")
     response = requests.get('https://kurlar.altin.in/guncel.asp')
@@ -22,3 +45,16 @@ while True:
     closes.append([now,float(usdttry_price['price']),float(kur)])
     closes = pd.DataFrame(closes,columns=['Time','Tether','Kur'])
     closes.to_sql('USDTTRY', engine, if_exists = 'append', index = False)
+    df = pd.read_sql("select * from USDTTRY", con=conn)
+    try:
+        if df.Tether.iloc[-1] < df.Kur.iloc[-1]:
+            if abs(df.Tether.iloc[-1] - df.Kur.iloc[-1]) / df.Tether.iloc[-1]>0.000006:
+                order_succeeded = order(SIDE_BUY, TRADE_QUANTITY, TRADE_SYMBOL)
+                if order_succeeded:
+                        print("Robot ilk tetherini aldı. Ademe girsin.")
+            else:
+                print("sell")
+    except:
+        print("error")
+
+
