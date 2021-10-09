1e25561c7da8c21858cb52ca93b333a32ce24bb1
mcaitjv <mucahitk@gmail.com>
2021-10-09 19:32:53 +0300

    init

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-API_KEY = 'your api key'
-API_SECRET = 'your api secret key'
+API_KEY = 'ClXIvMPkI1jEXxvJDU4e6HbCTO8yIlJ1Rf7WSNhIIpCR3vM1v8gGPe0BbBXAIijJ'
+API_SECRET = 'Gk8Z6w6unVHXRpfyov9YcLlGkGR0A3fxQzSHoApzccH7XITB7k1CIO6kSjYFUQfB'
